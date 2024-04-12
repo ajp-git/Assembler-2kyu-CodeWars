a2dece47ebf628cdb273ee5eaf07943c122c1eb8
Alain Jean PARES <ajp@free.fr>
2024-04-12 18:08:18 +0200

    Register functions

--- src/register.rs
+++ src/register.rs
@@ -1,3 +1,23 @@
 pub struct Register {
-    val:i32,
+    pub val:i32,
+}
+impl Register {
+    pub fn set_value(&mut self, new_val:i32 ) -> i32 {
+        self.val=new_val;
+        self.val
+    }
+
+    pub fn get_value(&self ) -> i32 {
+        self.val
+    }
+
+    pub fn inc(&mut self) -> i32 {
+        self.val+=1;
+        self.val
+    }
+
+    pub fn dec(&mut self) -> i32 {
+        self.val-=1;
+        self.val
+    }
 }
